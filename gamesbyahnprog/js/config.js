04a1c96111e221cc58b314d6fbe201a26d2012ff
ajh4500 <alexhenderson4500@gmail.com>
2026-04-27 11:39:17 -0500

    Update config.js

--- gamesbyahnprog/js/config.js
+++ gamesbyahnprog/js/config.js
@@ -1,3 +1,3 @@
 // Set this to your deployed backend URL when you go live.
 // Example: https://gamesbyahn-backend.onrender.com
-window.API_BASE = "";
+window.API_BASE = "https://gamesahn.onrender.com";
